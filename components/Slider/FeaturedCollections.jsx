4b4096de8d7f54718cf9ebced7f2491bcca66559
santohack <santohack238@gmail.com>
2024-03-16 03:14:34 +0530

    done

--- components/Slider/FeaturedCollections.jsx
+++ components/Slider/FeaturedCollections.jsx
@@ -10,7 +10,7 @@ import {
 } from "./EmblaCarouselArrowButtons";
 import useEmblaCarousel from "embla-carousel-react";
 
-const FeaturedCollections = (props) => {
+const CollectionUpdates = (props) => {
   const { images, options } = props; // Use 'images' instead of 'slides'
   const [emblaRef, emblaApi] = useEmblaCarousel(options);
 
@@ -25,48 +25,47 @@ const FeaturedCollections = (props) => {
   } = usePrevNextButtons(emblaApi);
 
   return (
-    <section className=" embla">
-      
-      <div className="embla__controls  ">
-        
-        <div className="embla__buttons pb-10 ">
-         <div className="rounded-full  border hover:bg-[#1C1C1D] hover:translate-x-0 border-gray-200">
-         <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
-         </div>
-         <div className="rounded-full  border hover:bg-[#1C1C1D] hover:translate-x-0 border-gray-200"> <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} /></div>
-         
-        </div>
-
-        {/* <div className="embla__dots text-gray-300">
-          {scrollSnaps?.map((_, index) => (
-            <DotButton
-              key={index}
-              onClick={() => onDotButtonClick(index)}
-              className={'text-gray-300 embla__dot'.concat(
-                index === selectedIndex ? 'text-gray-300  embla__dot--selected' : ''
-              )}
+    <section className="embla">
+      <div className="embla__controls">
+        <div className="embla__buttons right-0 pb-10">
+          <div className="rounded-full border hover:bg-[#1C1C1D] hover:translate-x-0 border-gray-200">
+            <PrevButton
+              onClick={onPrevButtonClick}
+              disabled={prevBtnDisabled}
             />
-          ))}
-        </div> */}
+          </div>
+          <div className="rounded-full border hover:bg-[#1C1C1D] hover:translate-x-0 border-gray-200">
+            <NextButton
+              onClick={onNextButtonClick}
+              disabled={nextBtnDisabled}
+            />
+          </div>
+        </div>
       </div>
       <div className="embla__viewport" ref={emblaRef}>
-        
-        <div className="embla__container  ">
+        <div className="embla__container">
           {images?.map((imageUrl, index) => (
-            <div className="embla__slide " key={index}>
-              <div className="rounded-3xl border py-10 hover:bg-[#1C1C1D] hover:translate-x-0 border-[#282829] flex items-center justify-center">
+            <div className="embla__slide" key={index}>
+              <div className="relative rounded-3xl border py-10 hover:bg-[#1C1C1D] hover:translate-x-0 border-[#282829] flex items-center justify-center">
                 <div className="text-center">
+                  <span className="block py-3 text-gray-500">
+                    Listend to autobook
+                  </span>
                   <img
                     src={imageUrl}
-                    
                     alt={`Slide ${index + 1}`}
                     className="h-[170px] bg-cover w-[100px] object-cover mx-auto mb-4"
                   />
-                  <span>Scythe (Arc of a Scythe, #1)</span>
-                  <span className="block text-gray-500">
-                    by Neal Shusterman
-                  </span>
                 </div>
+              </div>
+             
+              <div className=" flex-col relative inset-x-0 bottom-9 flex justify-center items-center">
+                <img
+                  src={imageUrl}
+                  alt={`Slide ${index + 1}`}
+                  className="h-[70px] bg-cover  border-4 border-[#080808] w-[70px] rounded-full object-cover mx-auto mb-4"
+                />
+                <span className="block pb-3">Scythe </span>
               </div>
             </div>
           ))}
@@ -76,4 +75,4 @@ const FeaturedCollections = (props) => {
   );
 };
 
-export default  FeaturedCollections;
+export default CollectionUpdates;
--- components/Slider/LatestReviews.jsx
+++ components/Slider/LatestReviews.jsx
@@ -26,46 +26,65 @@ const LatestReviews = (props) => {
 
   return (
     <section className=" embla">
-      
       <div className="embla__controls  ">
-        
         <div className="embla__buttons pb-10 ">
-         <div className="rounded-full  border hover:bg-[#1C1C1D] hover:translate-x-0 border-gray-200">
-         <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
-         </div>
-         <div className="rounded-full  border hover:bg-[#1C1C1D] hover:translate-x-0 border-gray-200"> <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} /></div>
-         
-        </div>
-
-        {/* <div className="embla__dots text-gray-300">
-          {scrollSnaps?.map((_, index) => (
-            <DotButton
-              key={index}
-              onClick={() => onDotButtonClick(index)}
-              className={'text-gray-300 embla__dot'.concat(
-                index === selectedIndex ? 'text-gray-300  embla__dot--selected' : ''
-              )}
+          <div className="rounded-full  border hover:bg-[#1C1C1D] hover:translate-x-0 border-gray-200">
+            <PrevButton
+              onClick={onPrevButtonClick}
+              disabled={prevBtnDisabled}
             />
-          ))}
-        </div> */}
+          </div>
+          <div className="rounded-full  border hover:bg-[#1C1C1D] hover:translate-x-0 border-gray-200">
+            {" "}
+            <NextButton
+              onClick={onNextButtonClick}
+              disabled={nextBtnDisabled}
+            />
+          </div>
+        </div>
       </div>
       <div className="embla__viewport" ref={emblaRef}>
-        
         <div className="embla__container  ">
           {images?.map((imageUrl, index) => (
             <div className="embla__slide " key={index}>
               <div className="rounded-3xl border py-10 hover:bg-[#1C1C1D] hover:translate-x-0 border-[#282829] flex items-center justify-center">
-                <div className="text-center">
-                  <img
-                    src={imageUrl}
+             
+                <div className=" flex  text-center">
+               
+                    <img
+                      src={imageUrl}
+                      alt={`Slide ${index + 1}`}
+                      className="h-[40px] w-[100px] bg-cover  rounded-full object-cover mb-4"
+                    />
+               
+                  <div className="flex flex-col justify-start ">
                     
-                    alt={`Slide ${index + 1}`}
-                    className="h-[170px] bg-cover w-[100px] object-cover mx-auto mb-4"
-                  />
-                  <span>Scythe (Arc of a Scythe, #1)</span>
-                  <span className="block text-gray-500">
-                    by Neal Shusterman
-                  </span>
+                    <img
+                      src={imageUrl}
+                      alt={`Slide ${index + 1}`}
+                      className="h-[170px] bg-cover w-[100px] object-cover mx-auto mb-4"
+                    />
+
+                    <span>Scythe (Arc of a Scythe, #1)</span>
+                    <span className="block text-gray-500">
+                      by Neal Shusterman
+                    </span>
+                    <div className="flex py-5  gap-2">
+                      <span>frida</span>
+                      <div className="w-1 h-1 bg-gray-300 mt-3 rounded-full flex items-center justify-center" />
+                      <span>16 Mar 2024</span>
+                    </div>
+                    
+                    <div>
+                      <h6>
+                        Reviews<span className=" text-center text-gray-500"> Vicious</span>{" "}
+                      </h6>
+                      <h6 className="text-center border-l ">
+                        It was easy and fast to read.
+                        It was easy and fast to read.It was easy and fast to read.
+                      </h6>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
